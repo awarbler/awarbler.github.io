af909a88009e38d5bc5baaf73feaa53d6a842c31
awarbler <awoodfor@byui.edu>
2019-10-10 17:00:52 -0500

    navigation blazzard way

--- lesson04/javascript04/scripts.js
+++ lesson04/javascript04/scripts.js
@@ -1,4 +1,3 @@
-function toggleMenu()
-{
-    document.getElementById("primaryNav").classList.toggle("hide");
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }

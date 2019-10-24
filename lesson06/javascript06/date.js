cd40547bd5fca4e8ccd1efca2cdcbdb08e943793
awarbler <awoodfor@byui.edu>
2019-10-24 18:59:45 -0500

    created new file 06

--- lesson06/javascript06/date.js
+++
@@ -1,7 +0,0 @@
-<script>
- datUpdated = new Date(document.lastModified) ;
- datMonth = datUpdated.getMonth() + 1 ;
- datDate = datUpdated.getDate() ;
- datYear = datUpdated.getYear() ;
- document.write("<i>Last Updated: " + datMonth + "/" + datDate + "/" + datYear + "</i>") ;
- </script>

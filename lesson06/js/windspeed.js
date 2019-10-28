7573ded5b6bf092e40b772b6607ff95eca648f87
awarbler <awoodfor@byui.edu>
2019-10-28 09:35:17 -0500

    trying to fix windchill and picture

--- lesson06/js/windspeed.js
+++ lesson06/js/windspeed.js
@@ -1,10 +1,10 @@
 // read a fahrenheit temperature from html document page 138 javascript book
 // programming fundamentals rex a breeze
 function doInputOutput(){
-var tempf= parseFloat(document.getElementById('high').textContent);
+var tempf = parseFloat(document.getElementById('high').textContent);
 var windSpeed = parseFloat(document.getElementById('wSpeed').textContent);
 var calculate = windChill(tempF, windspeed);
-if(tempf <= 50 && windSpeed > 3){
+if (tempf <= 50 && windSpeed > 3){
     // display the fahrenheit temperature to the user
 // using textContent instead since data is not html it is text 
 document.getElementById('chill').textContent=calculate;    
@@ -13,9 +13,10 @@ else{
     document.getElementById('chill').textContent=" N/A"
 }
 }
-function windChill(tempf, speed){
+function windChill(tempf, speed)
+{
     // convert the celsius temperature into fahrenheit
-     var f= 35.74 + 0.6215 * tempf - 35.75 * Math.pow(speed,0.16) + 0.4275 * tempf * Math.pow(speed,0.16).toFixed(2)
+     var f = (35.74 + (0.6215 * tempf)- (35.75 * Math.pow(speed,0.16)) +( 0.4275 * tempf * Math.pow(speed,0.16)).toFixed(2)
      return f;
  }
 

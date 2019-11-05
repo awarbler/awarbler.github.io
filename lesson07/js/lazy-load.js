a2fd81ee9b08b9a7bb7144bb705202393571226a
awarbler <awoodfor@byui.edu>
2019-11-05 04:48:50 -0600

    fixing progressive loading

--- lesson07/js/lazy-load.js
+++ lesson07/js/lazy-load.js
@@ -1,40 +1,39 @@
 // get all imgs with data - src attribute imagesToLoad
-const images = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 // optional parameters being set for the Intersectional Observer
 const imgOptions = {
-    threshold:0, 
+    threshold: 0,
     rootMargin: " 0px 0px 50px 0px"
 };
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
-}
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if (!src) {
-        return;
-    }
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
 
-    img.src = src; // actual img source
-}
+// first check to see if Intersection Observer is supported
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+            items.forEach((item) => {
+                if (item.isIntersecting) {
+                    loadImages(item.target);
+                    observer.unobserve(item.target);
+                }
+            });
+        },
+        imgOptions);
+    // all images inside of image we was it to observe the image. 
+    // apply observer for each image observe the image
+    images.forEach((img) => {
+
+        observer.observe(img);
 
 
-
-const imgObserver = new IntersectionObserver((entries,
-    imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
+    });
+    else {
+        images.forEach((img) =>){
+            loadImages(img);
+        });
         }
-    })
-
-}, imgOptions);
-
-// all images inside of image we was it to observe the image. 
-// apply observer for each image observe the image
-images.forEach(image => {
-imgObserver.observe(image);
-});

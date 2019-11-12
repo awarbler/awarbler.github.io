70f4dde65bf3d178c7a4cfe487eeaf0af0c67c44
awarbler <awoodfor@byui.edu>
2019-11-12 16:01:35 -0600

    name of files

---
+++ lesson09/js/lazy-load.js
@@ -0,0 +1,36 @@
+// get all imgs with data - src attribute imagesToLoad
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// optional parameters being set for the Intersectional Observer
+const imgOptions = {
+    threshold: 0,
+    rootMargin: " 0px 0px 50px 0px"
+};
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+// first check to see if Intersection Observer is supported
+if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+            items.forEach((item) => {
+                if (item.isIntersecting) {
+                    loadImages(item.target);
+                    observer.unobserve(item.target);
+                }
+            });
+        });
+        imagesToLoad.forEach((img) =>{
+            observer.observe(img);
+        });
+    // all images inside of image we was it to observe the image. 
+    // apply observer for each image observe the image
+    }
+    else {
+        imagesToLoad.forEach((img) =>{
+            loadImages(img);
+        });
+        }

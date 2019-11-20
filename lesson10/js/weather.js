29ed8819b3a913f053934eb19b0de6b55e273f8b
awarbler <awoodfor@byui.edu>
2019-11-19 18:21:57 -0600

    add temo

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -19,8 +19,11 @@ weatherObject.onload = function(){
     // data from the object is convert to variable formatted as json 
     // to look at the infor type console
     console.log(weatherInfo);
-    // get the document and the id element we created was place in single quotes set the dot inner html to weatherInfo it is a json object to we need to use values seperated by dot chrome tells us what to type go to chrome open up triangle and you will see to get the name we simply type name: 
+    // get the document and the id element we created was place in single quotes set the dot inner html to weatherInfo it is a json object to we need to use values seperated by dot chrome tells us what to type go to chrome open up triangle to access the data you will see to get the name we simply type name: 
+    // we are using ajax from the api 
     document.getElementById('place').innerHTML = weatherInfo.name;
+    // to get the current tempt on html- change id to currentTemp from id on html and change json reference
+    document.getElementById('currentTemp').innerHTML = weatherInfo.name;
 
 
 } // end of onload function 

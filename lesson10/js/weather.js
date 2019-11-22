55dd5bf509607938a1a5fec59e54488a5532e457
awarbler <awoodfor@byui.edu>
2019-11-22 17:01:47 -0600

    chg

--- lesson10/js/weather.js
+++ lesson10/js/weather.js
@@ -42,13 +42,11 @@ weatherObject.onload = function () {
     // should look like this create a const with icon_path as the variable you have quotes with the path, you have a + to concat ( to join 2 or more strings together) then the variable iconcode we created for the icon another + to close the concat then we glue on the last part with "" and we put the extension in the middle, make sure you add ; to close 
     const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
 
-
-
     // to check to see if icon_path works or will display we will put it in console
     console.log(icon_path);
 
-    // to add the image to html 
-    document.getElementById('weather_icon').src = icon_path;
+    // to add the image to html dont need for this project
+    //document.getElementById('weather_icon').src = icon_path;
 } // end of onload function 
 
 // forecast section use the forecast data use a query stringname this comes after a ? carry data over 
@@ -62,27 +60,46 @@ weatherObject.onload = function () {
 // use your own api no one elses
 
 const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=73cd33c03f2980c4fbeb11973fc5f602";
-
 // use fetch feed apiurl 
 // it will get the info for you and you tell me what to do in stages 
 fetch(apiURL)
     // here we are waiting for the info to come back 
     .then((response) => response.json())
-    // this sets up the object from the response 
+    // this sets up the object from the response this comes from the api documentation 
+    // arrow function is a shortcut it is a promise 
     .then((jsObject) => {
         // this lets us look at what we did 
         console.log(jsObject);
-        // this closes everything out nothing will print on screen only in console
-    });
-    // now we aregoing to go to the document and tell it change current-temp and change its text 
-    //content to jsobject information main.temp
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    console.log(current-temp);
-    document.getElementById('cityId').textContent = jsObject.city.name;
-    console.log(cityID);
 
-const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png'; // note the concatenation
-const desc = jsObject.weather[0].description; // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+        // this closes everything out nothing will print on screen only in console
+        // now we aregoing to go to the document and tell it change current-temp and change its text 
+        //content to jsobject information main.temp
+        // whatever we name here ' current-temp' has to match html file
+        document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
+        // document.getElementById('cityId').textContent = jsObject.name;
+
+        //image is a path the current weather is in a weather array 
+        // a zero based index use [] and the value is icon it has an absolute address 
+        // copied and pasted from the assignment - we have to declare an element 
+        // giving it a imagesrc as const here is the address wehre the img are stored 
+        // the openweatehrmap gives us then concat with a +
+        // jsOject is the label we gave it that is storing the information put a period to seperate
+        // the info, then we put .weather with an array (the file has an array so we have to go through
+        // the array to get the correct weather icon)then we put .icon (this is the name of the element 
+        // when we hover of the word icon in google chrome) then we put a +  then we add '.png'  because 
+        // this is the type of file we are dealing with and we have to notated it. 
+        // basically the file name
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[0].weather[0].icon + '.png'; // note the concatenation
+
+        // this gets the weather description if it sunny cloudy the alt will come from here 
+        // current weather conditions partyly cloudy set attribute for the alt
+        const desc = jsObject.list[0].weather[0].description; // note how we reference the weather array
+
+        // display the text of the source to make sure we concat it correctly 
+        //document.getElementById('imagesrc').textContent = imagesrc; // informational specification only
+        // document.getElementById('desc').textContent = desc; // this is my example to display icon descrption
+        // this is to put a alt for the image 
+        document.getElementById('icon').setAttribute('src', imagesrc); // focus on the setAttribute() method
+        // set the attribute for the image description 
+        document.getElementById('icon').setAttribute('alt', desc);
+    });

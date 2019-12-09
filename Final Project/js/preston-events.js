d21759a91f4ce14d60610009b0e0071d595c66cc
awarbler <awoodfor@byui.edu>
2019-12-09 16:10:45 -0600

    building new

---
+++ Final Project/js/preston-events.js
@@ -0,0 +1,29 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const towns = jsonObject['towns'];
+        for (let i=0; i<towns.length; i++) {
+            if (towns[i].name == 'Preston') {
+                let ecard = document.createElement('div');
+                let h4 = document.createElement('h4');
+                let p1 = document.createElement('p');
+                let p2 = document.createElement('p');
+                let p3 = document.createElement('p');
+
+                h4.textContent = "Upcoming Events";
+                p1.textContent = towns[i].events[0];
+                p2.textContent = towns[i].events[1];
+                p3.textContent = towns[i].events[2];
+
+                ecard.appendChild(h4);
+                ecard.appendChild(p1);
+                ecard.appendChild(p2);
+                ecard.appendChild(p3);
+                
+                document.querySelector('div.events').appendChild(ecard);
+
+            }
+        }});
---
+++ Final Project/js/webfontload.js
@@ -0,0 +1,5 @@
+WebFont.load({
+    google: {
+      families: ['open Sans', 'Roboto']
+    }
+  });

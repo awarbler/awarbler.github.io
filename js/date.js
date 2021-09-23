20297d08b7a6c4ec001091d1f59b9886b349c78a
awarbler <awoodfor@byui.edu>
2021-09-23 17:47:38 -0500

    up

--- js/date.js
+++ js/date.js
@@ -30,10 +30,9 @@ let fulldate = dayName + ',' + monthName + ' ' + d.getDate() + ',' + year;
 document.getElementById('currentdate').textContent = fulldate;
 
 
-<script>
+
  datUpdated = new Date(document.lastModified) ;
  datMonth = datUpdated.getMonth() + 1 ;
  datDate = datUpdated.getDate() ;
  datYear = datUpdated.getYear() ;
  document.write("<i>Last Updated: " + datMonth + "/" + datDate + "/" + datYear + "</i>") ;
- </script>

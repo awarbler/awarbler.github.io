2a35f86b3dfd3738d2c33c647362056b3fc2e7b3
awarbler <awoodfor@byui.edu>
2021-09-23 15:24:50 -0500

    update

--- wdd330/week1/js/main.js
+++ wdd330/week1/js/main.js
@@ -12,12 +12,3 @@ const list = document.querySelector('weeklyNotes');
 const listItems = listArray.map( (element) => {
     `<li id='${element} 'class='listItem'>${element}</li>` });
   list.innerHTML = listItems.join(''); -->*/
-
-  const btn = document.getElementById('button');
-
-  const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
-  
-  function change() {      
-    document.body.style.background = rainbow[Math.floor(7*Math.random())];
-  }
-  btn.addEventListener('click', change);
--- wdd330/week2/js/main.js
+++ wdd330/week2/js/main.js
@@ -3,7 +3,17 @@ function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
+ /* rainbow html*/
+
+ const btn = document.getElementById('button');
+
+ const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
  
+ function change() {      
+   document.body.style.background = rainbow[Math.floor(7*Math.random())];
+ }
+ btn.addEventListener('click', change);
+
  
  /* teamwork.html*/
  function functionOne() {

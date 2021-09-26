7353c32a25c902c74d43cc3dc1263baa2e248e76
awarbler <awoodfor@byui.edu>
2021-09-25 20:07:40 -0500

    up

--- wdd330/js/main.js
+++ wdd330/js/main.js
@@ -1,19 +1,4 @@
-/*--
-
-const links = [
-    {
-        label: 'Week1 notes',
-        url: 'wdd330/week1/index.html',
-    }
-]
-
-const list = document.querySelector('weeklyNotes');
-
-const listItems = listArray.map( (element) => {
-    `<li id='${element} 'class='listItem'>${element}</li>` });
-  list.innerHTML = listItems.join(''); -->*/
-
-
+// rainbow 
 const btn = document.getElementById('button');
 const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
 function change() {      
@@ -21,9 +6,47 @@ function change() {
 }
 btn.addEventListener('click', change);
 
+
+
+// table of contents
 const link = [{
     // the lae displays the "Week 1"
     label: "week 1",
     url: "wdd330/js/main.js",
 }
 ]
+// create an array to go through 
+const links = [
+    {
+    label: "Week 1",
+    url: "week1.index.html"
+    },
+    {
+        label: "Week 2",
+        url: "week2.index.html"
+    },
+    {
+        label: "Challenge: 1 Todo",
+        url: "week3.index.html"
+    }
+]
+
+/* create parent to create li 
+ * creatto go thrugh array 
+ * create li and a tags
+ * */
+
+var parent = document.getElementById("assignment")
+
+for (let i = 0; i < links.length; i++)
+{
+    const newList = document.createElement("li");
+    const link = document.createElement("a");
+    const text = document.createTextNode(link[i].label);
+
+    link.setAttribute("href", links[i].url);
+    link.appendChild(text);
+    newList.appendChild(link);
+    parent.appendChild(newList);
+
+}
--- wdd330/week1/js/main.js
+++ wdd330/week1/js/main.js
@@ -1,14 +1,1 @@
-/*--
 
-const links = [
-    {
-        label: 'Week1 notes',
-        url: 'wdd330/week1/index.html',
-    }
-]
-
-const list = document.querySelector('weeklyNotes');
-
-const listItems = listArray.map( (element) => {
-    `<li id='${element} 'class='listItem'>${element}</li>` });
-  list.innerHTML = listItems.join(''); -->*/

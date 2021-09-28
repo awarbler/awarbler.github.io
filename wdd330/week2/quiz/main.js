ca3626baac2a0fba833f9713938793b0caf3c3ab
awarbler <awoodfor@byui.edu>
2021-09-28 17:01:21 -0500

    updated week 2

--- wdd330/week2/quiz/main.js
+++ wdd330/week2/quiz/main.js
@@ -1,28 +1,33 @@
 //alert('Welcome to Quiz Ninja!');
-
-
 //this declares a variable called question and assigns the string 'What is Superman's real name?' to it.
 //const question = "What is Superman's real name?"
-
 /* ask the question stored in the quesitonvariable using a prompt dialog ,
  A prompt dialog allows the player to type in a response, which is then 
  stored in the variable it is assigned to, which is answer in this case.*/
 //const answer = prompt(question);
-
 /* use the alert diaglog to display the players answer using string 
 interpolation to insert eh value of the anser into the template literal 
 that is displayed in an alert box*/
 //alert(`You answered ${answer}`);
 
+
+
+
+
+
+window.onload = function() {
+  setTimeout(() => {
+    // put my code here 
 const quiz = [
     ["What is Superman's real name?","Clark Kent"],
     ["What is Wonder Woman's real name?","Diana Prince"],
     ["What is Batman's real name?","Bruce Wayne"]
 ];
 
-function start(quiz) {
+function start(quiz){
     // initialize score
     let score = 0;
+
     // main game loop
     for(const [question,answer] of quiz){
         const response = ask(question);
@@ -31,10 +36,12 @@ function start(quiz) {
     // end of main game loop
 
     gameOver();
+
     // function declarations
     function ask(question){
         return prompt(question);
     }
+
     function check(response,answer){
         // check if answer is correct
         if(response === answer){
@@ -45,9 +52,13 @@ function start(quiz) {
           alert(`Wrong! The correct answer was ${answer}`);
         }
       }
+
       function gameOver(){
         // At the end of the game, report the player's score
         alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
       }
 }
 start(quiz);
+
+}, 100);
+};

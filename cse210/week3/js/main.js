ef12488c4587265d3a83616e8f6280e77d2babaa
awarbler <awoodfor@byui.edu>
2021-10-05 14:00:17 -0500

    up

--- cse210/week3/js/main.js
+++ cse210/week3/js/main.js
@@ -1,41 +1,41 @@
-/** 1. select ol element
- *  2. Create an array of links
- *  3. Create li element and add an a tag with the label and url
-  */
- const links = [
-    {
-      label: "Week 1",
-      url: "wdd330/week1/index.html"
-    },
-    {
-        label: "Week 2",
-        url: "wdd330/week2/index.html"
-    },
-    {
-        label: "Week 3",
-        url: "wdd330/week3/index.html"
-    },
-    {
-        label: "Week 4",
-        url: "cse210/week04/index.html"
-    }
+// /** 1. select ol element
+//  *  2. Create an array of links
+//  *  3. Create li element and add an a tag with the label and url
+//   */
+//  const links = [
+//     {
+//       label: "Week 1",
+//       url: "wdd330/week1/index.html"
+//     },
+//     {
+//         label: "Week 2",
+//         url: "wdd330/week2/index.html"
+//     },
+//     {
+//         label: "Week 3",
+//         url: "wdd330/week3/index.html"
+//     },
+//     {
+//         label: "Week 4",
+//         url: "cse210/week04/index.html"
+//     }
     
-  ]
+//   ]
 
-  /**create for loop to go through array and create li and a tags */
+//   /**create for loop to go through array and create li and a tags */
 
-  var parent = document.getElementById("assignments")
+//   var parent = document.getElementById("assignments")
   
-  for(let i = 0; i < links.length; i++){
-    const newList = document.createElement("li");
-    const link = document.createElement("a");
-    const text = document.createTextNode(links[i].label);
+//   for(let i = 0; i < links.length; i++){
+//     const newList = document.createElement("li");
+//     const link = document.createElement("a");
+//     const text = document.createTextNode(links[i].label);
 
-    link.setAttribute("href", links[i].url);
-    link.appendChild(text);
-    newList.appendChild(link);
-    parent.appendChild(newList);
-  }
+//     link.setAttribute("href", links[i].url);
+//     link.appendChild(text);
+//     newList.appendChild(link);
+//     parent.appendChild(newList);
+//   }
 /* create an array to go through 
 const links = [
     {

7e634062b0de44ab8ed4fdcdf3dcb83cbeec9025
awarbler <awoodfor@byui.edu>
2021-10-04 22:15:04 -0500

    cse210 week 4

---
+++ cse210/week04/video tutorial/OOPExample.py
@@ -0,0 +1,30 @@
+# travel OOP or object oriented programming
+''' 
+Unit - class
+centered on creating objects
+Object:  A single software unit that combines data and methods
+Data in an object are known as attributes
+Procedures / functions in an object are known as methods
+
+'''
+# class a blueprint to have dada and methods
+
+
+#create class called cab
+class Cab {
+    cabService, make, location, numberPlate # data
+    book(), arrival(), start() #methods
+}
+
+# create class called cabDriver
+
+class CabDrive {
+    name, employeeId #data
+    openDoor(), drive() #methods
+    }
+
+# create class called passenger
+class passenger {
+    name, address #data
+    openApp(), bookCab(),walk() #methods
+}

a5a7a51db233dd6b408345be8f4f93bc6b8457e6
awarbler <awoodfor@byui.edu>
2021-10-12 22:06:08 -0500

    up

--- cse210/week04/dice/game/thrower.py
+++ cse210/week04/dice/game/thrower.py
@@ -12,24 +12,22 @@ To print the value, use the print() function
 # TODO: Define the Thrower class here.
 
 class Thrower:
- """A code template for a person who participates. The responsibility of 
+    """A code template for a person who participates. The responsibility of 
     this class of objects is to check to see if they can throw the dice, 
     have themthrow the dice, and get points
     
     Attributes:
         dice[5]: track the number of dice that is being thrown
-        number_throws: The total number of throws the user threw
-       
+        number_throws: The total number of throws the user threw    
     """
 
- #The class constructor.  
-
-def __init__(self):
+    #The class constructor.  
+    def __init__(self):
         self.dice = []
         self.num_throws = 0
 
-# determines if the thrower  can throw again
-"""Determines whether or not the Thrower can throw again according to 
+    # determines if the thrower  can throw again
+    """Determines whether or not the Thrower can throw again according to 
     the rules. 
         Args: 
             self (Thrower): An instance of Thrower.
@@ -38,12 +36,12 @@ def __init__(self):
             boolean: True if the list of dice has at least a five, or a one, or 
             the number of throws is zero; false if otherwise.
         """
-def can_throw(self):
-       return (self.dice.count(5) > 0 or self.dice.count(1) > 0 
+    def can_throw(self):
+        return (self.dice.count(5) > 0 or self.dice.count(1) > 0 
                 or self.num_throws == 0)
-# determine 
-def get_points(self):
- """Calculates the total number of points for the current throw. Fives 
+    # determine 
+    def get_points(self):
+        """Calculates the total number of points for the current throw. Fives 
         are worth 50 points. Ones are worth 100 points. 
         Args: 
             self (Thrower): An instance of Thrower.
@@ -51,14 +49,14 @@ def get_points(self):
         Returns:
             number: The total points for the current throw.
         """        
- return self.dice.count(5) * 50 + self.dice.count(1) * 100 
-# define throw_dice 
-# throw dice
-"""Throws the dice by randomly generating five new values. 
+        return self.dice.count(5) * 50 + self.dice.count(1) * 100 
+    # define throw_dice 
+    # throw dice
+    """Throws the dice by randomly generating five new values. 
         Args: 
             self (Thrower): An instance of Thrower.
         """
-def throw_dice(self):
+    def throw_dice(self):
         self.dice.clear()
         for i in range(5):
                 result = random.randint(1,6)
---
+++ cse210/week5/soloWork/hide_and_seek/game/hider.py
@@ -0,0 +1,69 @@
+import random
+
+class Hider:
+# TODO: Define the Hider class here
+    """A code template for the hider. The responsibility of this class of objects
+    is hide from the seeker and to give hints of location.
+    
+    Stereotype:
+        Information Holder
+
+    Attributes:
+        location (integer): The location of the hider (1-1000). 
+        distance (list): The distance travelled with each move. """
+  
+    def __init__(self):
+        self.location= random.randint(1,1000) # the location of the hider 
+        self.distance = [0, 0] # start with two so get_message always works
+
+    
+    '''The watch method keeps track of how far away the seeker is by calculating 
+    the difference between their locations. The distance is appended to the corresponding 
+    attribute for later use. .'''
+
+    def watch(self,location):
+        """Moves to the given location and keeps track of the distance.
+
+        Args:
+            self (Seeker): An instance of Seeker.
+        """
+
+        distance = abs(self.location - location)
+        print (distance)
+
+        #self.distance.append(watch)
+        #self.location = location        
+
+
+# The Hider class must have two methods called get_hint, and watch. 
+# Watch accepts one argument called location.
+
+
+'''The get_hint method returns a hint that depends on whether or not the 
+seeker has moved closer or farther away. This is determined by inspecting 
+the last two distances contained in the distance attribute.''' 
+
+# def get_hint(self):
+
+
+#         # The program interface must resemble the sample provided above.
+
+#     """Gets a hint from the hider.
+
+#         Args:
+#             self (Hider): An instance of Hider.
+        
+#         Returns:
+#             string: A hint from the hider
+#         """
+#     hint= "\nI'm going to find you!"    
+#     if self.distance[-1] == 0:
+#             hint = "\nYou found me!"
+#     elif self.distance[-1] < self.distance[-2]:
+#             hint= "\nGetting warmer!"
+#     elif self.distance[-1] > self.distance[-2]:
+#             hint= "\nGetting colder!"
+#     return message
+   
+
+

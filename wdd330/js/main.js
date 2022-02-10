6d52a0c42a5c5bcd17a77d32f61fda65a8d36fed
awarbler <awoodfor@byui.edu>
2022-02-10 11:40:27 -0600

    updating

--- wdd330/js/main.js
+++
@@ -1,9 +0,0 @@
-// rainbow 
-const btn = document.getElementById('button'); // create a button 
-const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
-function change() {      
-    document.body.style.background = rainbow[Math.floor(7*Math.random())];
-}
-btn.addEventListener('click', change);
-
-
